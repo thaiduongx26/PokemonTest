649aa62f56a867bca96b387f3f0a048da3ce17df
Thai Duong <thaiduong@macbook-pro.local>
2017-02-08 04:40:56 -0800

    Initial Commit

---
+++ PokemonTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonTest
+//
+//  Created by Thai Duong on 2/8/17.
+//  Copyright © 2017 Thai Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PokemonTestTests/PokemonTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  PokemonTestTests.swift
+//  PokemonTestTests
+//
+//  Created by Thai Duong on 2/8/17.
+//  Copyright © 2017 Thai Duong. All rights reserved.
+//
+
+import XCTest
+@testable import PokemonTest
+
+class PokemonTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
